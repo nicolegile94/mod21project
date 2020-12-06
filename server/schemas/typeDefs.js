49bbed42a62d8f88674ced5810978e6254482626
Nicole Gile <nicolegile94@gmail.com>
2020-12-06 17:26:13 -0600

    add and remove books mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,18 +3,18 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
 
   type User {
-    _id: ID
-    username: String
+    _id: ID!
+    username: String!
     email: String
     bookCount: Int
     savedBooks: [Book]
   }
 
   type Book {
-    bookId: Int
+    bookId: ID!
     authors: [String]
     description: String
-    title: String
+    title: String!
     image: Img
     link: Link
   }
@@ -23,17 +23,17 @@ const typeDefs = gql`
     token: ID!
     user: User
   }
-  
+
   type Query {
-    me(username: String): [User]
+    me: User
   }
 
 
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(authors: Array!, description: String!, title: String!, bookId: Int!, image: Int!, link: String!): User
-    removeBook(bookId: Int!) :  User
+    saveBook(authors: [String], description: String!, title: String!, bookId: String!, image: String, link: String): User
+    removeBook(bookId: ID!) :  User
   }
 `;
 module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+
 
 const path = require('path');
 const { typeDefs, resolvers } = require('./schemas');
@@ -10,7 +12,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 server.applyMiddleware({ app });
@@ -19,9 +22,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-//if (process.env.NODE_ENV === 'production') {
-//  app.use(express.static(path.join(__dirname, '../client/build')));
-//}
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 //app.use(routes);
 
