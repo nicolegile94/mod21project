09a35d31b7ba72134cfa89cf23d673701be5d002
Nicole Gile <nicolegile94@gmail.com>
2020-12-06 15:45:09 -0600

    added user and book types

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,12 @@
+const { UserInputError } = require("apollo-server-express");
+const { User } = require('../models');
+
 const resolvers = {
     Query: {
-      helloWorld: () => {
-        return 'Hello world!';
-      }
+      users: async (parent, { username }) => {
+        const params = username ? { username } : {};
+        return User.find();
+      },
     }
   };
   
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,26 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: Int
+    authors: [String]
+    description: String
+    title: String
+    image: Img
+    link: Link
+  }
+
   type Query {
-    helloWorld: String
+    me(username: String): [User]
   }
 `;
 module.exports = typeDefs;
