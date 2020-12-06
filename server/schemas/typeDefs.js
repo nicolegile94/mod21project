505f215ccb2bd318ab6bf566776200020f79e33a
Nicole Gile <nicolegile94@gmail.com>
2020-12-06 14:40:13 -0600

    implement apollo and apply it

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,8 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql `
-    type Query {
-        
-    }`;
-
+const typeDefs = gql`
+  type Query {
+    helloWorld: String
+  }
+`;
 module.exports = typeDefs;
